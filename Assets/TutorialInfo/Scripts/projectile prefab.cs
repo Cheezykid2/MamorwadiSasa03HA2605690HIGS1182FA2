332f7eb847af0966815dac58b10c16555a7ddabe
Cheezykid2 <kidpeace1017@gmail.com>
2026-09-06 14:25:51 +0200

    added scenes

---
+++ Assets/TutorialInfo/Scripts/projectile prefab.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public GameObject projectilePrefab;
+
+void Update()
+{
+	if (Input.GetKeyDown(KeyCode.Space))
+	{
+		Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+	}
+}
+
+public Class1()
+	{
+	}
+}
