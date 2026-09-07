fe573d1087c0494c3f0afcc702a603d502605f3e
Cheezykid2 <kidpeace1017@gmail.com>
2026-09-07 16:06:59 +0200

    created the menu scene and script

--- Assets/TutorialInfo/Scripts/projectile prefab.cs
+++ Assets/TutorialInfo/Scripts/projectile prefab.cs
@@ -1,16 +1,1 @@
-﻿using System;
-
-public GameObject projectilePrefab;
-
-void Update()
-{
-	if (Input.GetKeyDown(KeyCode.Space))
-	{
-		Instantiate(projectilePrefab, transform.position, Quaternion.identity);
-	}
-}
-
-public Class1()
-	{
-	}
-}
+﻿using UnityEngine;
